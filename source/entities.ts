34eeda06408259e44beaee3e6c1b78af53dee063
Ilya Pogrebensky <lentyai@gmail.com>
2022-10-22 19:33:56 +0300

    Broken server

---
+++ source/entities.ts
@@ -0,0 +1,41 @@
+import { Request } from 'express';
+import { AppError, Role } from "./enums";
+
+export interface entityWithId {
+    id: number;
+}
+
+export interface whiteBoardType extends entityWithId {
+    type: string;
+}
+
+export interface systemError {
+    key: AppError;
+    code: number;
+    message: string;
+}
+
+export interface sqlParameter {
+    name: string;
+    type: any;
+    value: string | number;
+}
+
+export interface authenticationToken {
+    userData: jwtUserData;
+}
+
+export interface jwtUserData {
+    userId: number;
+    roleId: Role;
+    loginDate: Date;
+}
+
+export interface AuthenticatedRequest extends Request, authenticationToken { }
+
+export interface user extends entityWithId {
+    firstName: string;
+    lastName: string;
+    login?: string;
+    password?: string;
+}
---
+++ source/middleware/auth.middleware.ts
@@ -0,0 +1,35 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from 'express';
+import { TOKEN_SECRET } from "../constants";
+import { Role } from "../enums";
+import { AuthenticatedRequest, jwtUserData } from "../entities";
+
+interface jwtBase {
+    userData: jwtUserData;
+    exp: number;
+    iat: number;
+}
+
+const verifyToken = (roles: Role[]) => (req: Request, res: Response, next: NextFunction) => {
+    const token: string | undefined = req.headers["authentication"]?.toString();
+
+    if (!token) {
+        return res.status(403).send("A token is required for authentication");
+    }
+
+    try {
+        // 'Bearer ..............'
+        token = token.substring("Bearer ".length);
+        const decoded: string | JwtPayload = jwt.verify(token, TOKEN_SECRET);
+        if (roles.indexOf((decoded as jwtBase).userData.roleId) === -1) {
+            return res.sendStatus(401);
+        } 
+        (req as AuthenticatedRequest).userData = (decoded as jwtBase).userData;
+    } catch (err) {
+        return res.status(401).send("Invalid Token");
+    }
+
+    return next();
+};
+
+export default { verifyToken };
